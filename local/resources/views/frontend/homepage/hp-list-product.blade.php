52cc9fcf6fe15d362db4b6f503a36b3979b072c6
QUANG_CTY <nnduyquang@gmail.com>
2017-12-21 16:48:25 +0700

    ok

--- local/resources/views/frontend/homepage/hp-list-product.blade.php
+++ local/resources/views/frontend/homepage/hp-list-product.blade.php
@@ -1,1 +1,63 @@
-list product
+<div id="hp-content-list-product" class="col-md-12">
+    <div class="row">
+        <div class="container">
+            <div class="row">
+                <div class="one-category">
+                    <div class="title-top">
+                        <h3 class="header-title">
+                            <span><a href="#">Đèn Phòng Ngủ</a></span>
+                            <a class="view-all" href="#">Xem Tất Cả</a>
+                        </h3>
+                        {{ Html::image('images/temp/temp_list_product_banner.png','',array('class'=>'no-style')) }}
+                    </div>
+                    <div class="list-item">
+                        <a href="#">
+                            <div class="one-item col-md-3">
+                                <div class="wrap-item">
+                                    {{ Html::image('images/temp/temp_hp_item.jpg','',array('class'=>'no-style')) }}
+                                    <div class="item-detail">
+                                        <span class="title">Bút Bi Thiên Long</span>
+                                        <span class="price">1.000.000</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="one-item col-md-3">
+                                <div class="wrap-item">
+                                    {{ Html::image('images/temp/temp_hp_item.jpg','',array('class'=>'no-style')) }}
+                                    <div class="item-detail">
+                                        <span class="title">Bút Bi Thiên Long</span>
+                                        <span class="price">1.000.000</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="one-item col-md-3">
+                                <div class="wrap-item">
+                                    {{ Html::image('images/temp/temp_hp_item.jpg','',array('class'=>'no-style')) }}
+                                    <div class="item-detail">
+                                        <span class="title">Bút Bi Thiên Long</span>
+                                        <span class="price">1.000.000</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="one-item col-md-3">
+                                <div class="wrap-item">
+                                    {{ Html::image('images/temp/temp_hp_item.jpg','',array('class'=>'no-style')) }}
+                                    <div class="item-detail">
+                                        <span class="title">Bút Bi Thiên Long</span>
+                                        <span class="price">1.000.000</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
