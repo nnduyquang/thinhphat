4b9d23d8ed952566f28fec55f8ffebc0895efdaa
nnduyquang@gmail.com <bunvodich>
2018-06-30 11:18:56 +0700

    ok

--- local/resources/views/frontend/master.blade.php
+++ local/resources/views/frontend/master.blade.php
@@ -44,12 +44,24 @@
 </script>
 @yield('scripts')
 {{ Html::script('js/scripts.js') }}
-<div class="callback hidden-lg hidden-md">
+<div class="callback d-lg-none d-md-none">
     <div class="phone_animation">
         <div class="phone_animation_circle"></div>
         <div class="phone_animation_circle_fill"></div>
         <a href="tel:0965322239" class="phone_animation_circle_fill_img"><i class="fa fa-phone" aria-hidden="true"></i></a>
     </div>
+</div>
+<div class="callback d-none d-md-block" style="right: 150px;bottom: -30px;left:inherit">
+    <div class="phone_animation">
+        <div class="phone_animation_circle"></div>
+        <div class="phone_animation_circle_fill"></div>
+        <a href="tel:0965322239" class="phone_animation_circle_fill_img"><i class="fas fa-phone"
+                                                                                                   aria-hidden="true"></i></a>
+    </div>
+</div>
+<div class="mess_desk_bot d-none d-md-block" style="position: fixed;bottom:40px;right: 0px;">
+    <a href="tel:0965322239" style="display: block;width: 260px;height: 56px;background: url({{URL::to('images/nenhot.png')}}) no-repeat;text-align: center;padding-top: 10px;color:#fff;font-size: 20px;font-family: 'roboto-bold'">
+    </a>
 </div>
 <!--End of Tawk.to Script-->
 
