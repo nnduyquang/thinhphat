0f3b15b92758b95743588c834f41f93adf3a8061
nnduyquang@gmail.com <bunvodich>
2018-10-15 17:05:13 +0700

    ok

--- local/resources/views/frontend/common/footer/footer.blade.php
+++ local/resources/views/frontend/common/footer/footer.blade.php
@@ -11,9 +11,9 @@
                     <h3>{{ Html::image('images/uploads/logo2f.png','',array('style'=>'width:80%;height:auto;margin-top:-28px')) }}</h3>
                     <ul class="fe_contact">
 
-                        <li><a href="#"><i class="fas fa-map-marker"></i><span>ĐỊA CHỈ : 55/4 KP3 Hà Huy Giáp, Q.12, TPHCM</span></a>
+                        <li><a href="#"><i class="fas fa-map-marker"></i><span>ĐỊA CHỈ : {{$dataConfig['config-address']}}</span></a>
                         </li>
-                        <li><a href="#"><i class="fas fa-users"></i><span>LIÊN HỆ : 0901.196.676 </span></a></li>
+                        <li><a href="#"><i class="fas fa-users"></i><span>LIÊN HỆ : {{$dataConfig['config-phone']}} </span></a></li>
                         <li><a href=""><i
                                         class="far fa-clock"></i><span>Từ 8h00-17h00 các ngày thứ 2 đến thứ 7</span></a>
                         </li>
@@ -24,13 +24,7 @@
 
                 <div class="col-md-4 mb-3">
                     <h3>BẢN ĐỒ <span>HƯỚNG DẨN</span></h3>
-                    <iframe
-                            width="100%"
-                            height="186px"
-                            frameborder="0" style="border:0"
-                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCz9f8H5wjJIac5LrePbowoDN8Vp2FEEZ8
-    &q=55+Hà+Huy+Giáp,+Q.12,+TPHCM&zoom=17" allowfullscreen>
-                    </iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.3249366789546!2d106.63012575078301!3d10.862871592224957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529f433b302c7%3A0x85d2b4c95f4c1de7!2zMTYgVMOibiBDaMOhbmggSGnhu4dwIDEwLCBUw6JuIENow6FuaCBIaeG7h3AsIFF14bqtbiAxMiwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1539597511695" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
 
 
